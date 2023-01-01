abc46bd83ba95dd5831a9dad0ec6bbe9f2a1e66e
AndySrb <AndySrb@protonmail.com>
2023-01-01 16:10:11 +0000

    Picom transparency program config

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-cp -u -r ../.vim ../.vimrc ../.bashrc ../.config/i3 ../.config/polybar ../.config/rofi ../.config/mpd ../.config/mpv ../.config/zathura ../.rtorrent.rc ../.fehbg ../Media/Wallpapers ../.config/ranger ./.
+cp -u -r ../.vim ../.vimrc ../.bashrc ../.config/i3 ../.config/polybar ../.config/rofi ../.config/mpd ../.config/mpv ../.config/zathura ../.rtorrent.rc ../.fehbg ../Media/Wallpapers ../.config/ranger ../.config/picom  ./.
